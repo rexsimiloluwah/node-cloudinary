0a5e3daa346455a7c59dc82ce031cbd9fa4c5596
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-12-23 23:19:29 +0000

    User controllers and CRUD endpoints

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,12 @@
  * @author [Similoluwa Okunowo (The Caveman)]
  * @email [rexsimiloluwa@gmail.com]
  * @create date 2020-12-20 01:49:23
- * @modify date 2020-12-20 03:36:02
+ * @modify date 2020-12-20 17:09:09
  * @desc [User's controller]
  */
 
- const multerUploads = require('../middlewares/multer');
-
+const multerUpload = require('../middlewares/multer');
+const cloudinary = require('../config/cloudinaryConfig');
 const db = require('../models');
 const HttpError = require('../HttpError');
 const slugify = require('slugify');
@@ -19,6 +19,21 @@ const Op = db.Sequelize.Op;
 const registerUser = async (req,res,next) => {
     // console.log(req.file);
     // console.log(req.body);
+    let result;
+    if(req.file){
+        try{
+            // Upload image to cloudinary
+            result = await cloudinary.uploader.upload(req.file.path);
+            console.log(result)
+        }
+        catch(err){
+            return res.status(500)
+                    .json({
+                        "message" : err
+                    })
+        }
+    }
+
     const user = await Users.findAll({where : {email : req.body.email}});
     console.log(user)
 
@@ -36,7 +51,7 @@ const registerUser = async (req,res,next) => {
         linkedin : req.body.linkedin,
         twitter : req.body.twitter,
         slug : slugify(req.body.name).toLowerCase(),
-        picture : "https://0.academia-photos.com/73022480/49009756/37024547/s200_okunowo.simi.jpg"
+        picture : result ? result.secure_url : ""
     }
 
     Users.create(newUser)
@@ -90,6 +105,94 @@ const getUserBySlug = async (req, res, next) => {
     })
 }
 
+// @desc Controller for updating user details 
+const updateUserBySlug = async (req, res, next) => {
+    console.log(req.params.slug);
+
+    const user = await Users.findAll({where : {slug : req.params.slug}});
+
+    if(user.length === 0){
+        return res.status(400)
+            .json({
+                "message" : "User not found."
+            })
+    }
+
+    let result;
+    if(req.file){
+        try{
+            // Upload image to cloudinary
+            result = await cloudinary.uploader.upload(req.file.path);
+            console.log(result)
+        }
+        catch(err){
+            return res.status(500)
+                    .json({
+                        "message" : err
+                    })
+        }
+    }
+
+    const newData = {}
+    for(const property in req.body){
+        newData[property] = req.body[property];
+    }
+    if(newData.name){
+        newData["slug"] = slugify(newData.name).toLowerCase();
+    }
+    if(result){
+        newData['picture'] = result.secure_url;
+    }
+
+    Users.update(newData, {
+            where : {slug : req.params.slug}
+        })
+        .then(response => {
+            console.log(response);
+            res.status(200)
+            .json({
+                "message" : "Successfully updated user"
+            })
+        })
+        .catch(err => {
+            console.error(err);
+            next(new HttpError(err, 500));
+        })
+
+
+}
+
+// @desc Controller for deleting existing user
+const deleteUserBySlug = async (req, res, next) => {
+    console.log(req.params.slug);
+
+    const user = await Users.findAll({where : {slug : req.params.slug}});
+
+    if(user.length === 0){
+        return res.status(400)
+            .json({
+                "message" : "User not found."
+            })
+    }
+
+    Users.destroy({
+        where : {slug : req.params.slug}
+    })
+    .then(response => {
+        console.log(response);
+        res.status(200)
+            .json({
+                "message" : "User successfully deleted from Database."
+            })
+    })
+    .catch(err => {
+        console.error(err);
+        next(new HttpError(err, 500));
+    })
+}
+
 module.exports.registerUser = registerUser;
 module.exports.getUserBySlug = getUserBySlug;
 module.exports.getAllUsers = getAllUsers;
+module.exports.updateUserBySlug = updateUserBySlug;
+module.exports.deleteUserBySlug = deleteUserBySlug;
